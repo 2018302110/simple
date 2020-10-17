0910d043281472b5cb8229564798653ebc837300
2018302110 <70925142+2018302110@users.noreply.github.com>
2020-10-17 10:52:14 +0800

    Add files via upload

---
+++ 文本统计分析程序/WindowsFormsApp1/WindowsFormsApp1/Custom.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public struct customWord
+    {
+        public int num;
+        public string word;
+    }
+
+    class Custom
+    {
+        //判断词语是否重复
+        public bool isRepet(string custom, ArrayList theWordsWord)
+        {
+            for (int i = 0; i < theWordsWord.Count; i++)
+            {
+                if (custom == (string)theWordsWord[i])
+                {
+                    string a = "这个词语重复了";
+                    string b = "提示";
+                    MessageBox.Show(a, b);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //判断输入是否只有汉字
+        public bool isLegal(string custom)
+        {
+            for(int i=0;i<custom.Length;i++)
+            {
+                //不是汉字
+                if (custom[i] < 0x4e00 || custom[i] > 0x9fbb)
+                {
+                    string a = "词语不能包含非汉字哦";
+                    string b = "提示";
+                    MessageBox.Show(a, b);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //判断是否输入了
+        public bool isInput(string custom)
+        {
+            if (custom.Length == 0)
+            {
+                string a = "请输入自定义词";
+                string b = "提示";
+                MessageBox.Show(a, b);
+                return false;
+            }
+            return true;
+        }
+
+        //得出自定义词数量，更新输出
+        public customWord[] countWord(string custom,string allWords2,ArrayList theWordsNum, ArrayList theWordsWord)
+        {
+            if (isRepet(custom, theWordsWord)&&isLegal(custom)&&isInput(custom))
+            {
+                theWordsWord.Add(custom);
+                theWordsNum.Add(0);
+                allWords2.ToCharArray();
+                custom.ToCharArray();
+
+                //得出文本中自定义词的数量
+                for (int i = 0; i < allWords2.Length; i++)
+                {
+                    if (allWords2[i] == custom[0])
+                    {
+                        for (int n = 0; n < custom.Length; n++)
+                        {
+                            if (allWords2[i + n] != custom[n])
+                                break;
+                            if (n == (custom.Length - 1))
+                                theWordsNum[theWordsNum.Count - 1] = Convert.ToInt32(theWordsNum[theWordsNum.Count - 1]) + 1;
+                        }
+                    }
+                }
+            }
+
+            customWord[] customword = new customWord[theWordsWord.Count];
+
+            for (int i = 0; i < theWordsWord.Count; i++)
+            {
+                customword[i].num = Convert.ToInt32(theWordsNum[i]);
+                customword[i].word = (string)theWordsWord[i];
+            }
+
+            return customword;
+        }
+
+        //将词语按照数量由多到少排序
+        public customWord[] getTheSort2(customWord[] getIn)
+        {
+            for (int i = 0; i < getIn.Length; i++)
+            {
+                int max = getIn[i].num;
+                int maxNum = 0;
+                for (int c = i; c < getIn.Length; c++)
+                {
+                    if (max < getIn[c].num)
+                    {
+                        max = getIn[c].num;
+                        maxNum = c;
+                    }
+                }
+                //当有更大的才交换
+                if (maxNum != 0)
+                {
+                    int ii; string iii;
+                    ii = getIn[i].num;
+                    getIn[i].num = getIn[maxNum].num;
+                    getIn[maxNum].num = ii;
+                    iii = getIn[i].word;
+                    getIn[i].word = getIn[maxNum].word;
+                    getIn[maxNum].word = iii;
+                }
+            }
+            return getIn;
+        }
+
+    }
+}
---
+++ 文本统计分析程序/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using WindowsFormsApp1;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //start按钮
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartPage startPage = new StartPage();
+            ShowResult showResult = new ShowResult();
+            //用户输入的初始文本
+            string allWords1 = textBox1.Text;
+            //返回排列好的结构体数组
+            theWord[] theWords2 = startPage.start(allWords1);
+            //在结果page上显示结果
+            textBox2.Text = showResult.wordResult(theWords2);
+            textBox3.Text = showResult.numResult1(theWords2);
+            textBox4.Text = showResult.numResult2(theWords2);
+        }
+
+
+        //自定义页须将中间过程保存在button方法外面
+        //存储每种字的数量
+        ArrayList theWordsNum = new ArrayList();
+        //存储所有字的种类
+        ArrayList theWordsWord = new ArrayList();
+
+        //自定义添加按钮
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //用户输入的自定义词
+            string customWord = textBox5.Text;
+
+            StartPage startPage = new StartPage();
+            ShowResult showResult = new ShowResult();
+            Custom custom = new Custom();
+
+            //用户输入的初始文本
+            string allWords1 = textBox1.Text;
+            //返回去除非汉字的文本
+            string allWords2 = startPage.removeNonWords(allWords1);
+
+            //得出自定义词数量，更新输出词组
+            customWord[] customWords=custom.countWord(customWord, allWords2, theWordsNum, theWordsWord);
+            //将词语按照数量由多到少排序
+            customWords = custom.getTheSort2(customWords);
+
+            //在自定义page上显示结果
+            textBox6.Text = showResult.wordResult(customWords);
+            textBox7.Text = showResult.numResult1(customWords);
+            textBox8.Text = showResult.numResult2(customWords);
+
+        }
+
+
+        //关联词语页须将中间过程保存在button方法外面
+        //存储关联词的关联次数
+        ArrayList relevanceWordsNum = new ArrayList();
+        //存储所有关联词的种类
+        ArrayList relevanceWordsWord = new ArrayList();
+
+        //关联页查看关联按钮
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StartPage startPage = new StartPage();
+            ShowResult showResult = new ShowResult();
+            Relevance relevance = new Relevance();
+
+            //用户输入的关联词
+            string word1 = textBox9.Text;
+            string word2 = textBox10.Text;
+
+            //用户输入的初始文本
+            string allWords1 = textBox1.Text;
+            //返回去除非汉字的文本
+            string allWords2 = startPage.removeNonWords(allWords1);
+
+            //获得关联度的标准
+            int criterion = 0;
+            if (radioButton1.Checked)
+                criterion = 3;
+            if (radioButton2.Checked)
+                criterion = 6;
+            if (radioButton3.Checked)
+                criterion = 10;
+            if (radioButton4.Checked&&relevance.isNum(textBox11.Text))
+                criterion = int.Parse(textBox11.Text);
+
+            //得出关联词关联度
+            relevanceWord[] relevanceword = relevance.getRelevance(word1, word2, allWords2, criterion, relevanceWordsNum, relevanceWordsWord);
+            
+            //在关联页上显示结果
+            textBox13.Text= showResult.wordResult(relevanceword);
+            textBox12.Text = showResult.numResult1(relevanceword);
+            textBox14.Text = showResult.numResult2(relevanceword);
+
+        }
+
+        //清除关联页的结果
+        private void button4_Click(object sender, EventArgs e)
+        {
+            relevanceWordsNum.Clear();
+            relevanceWordsWord.Clear();
+            textBox13.Text = null;
+            textBox12.Text = null;
+            textBox14.Text = null;
+        }
+
+        //清除自定义页的结果
+        private void button5_Click(object sender, EventArgs e)
+        {
+            theWordsNum.Clear();
+            theWordsWord.Clear();
+            textBox6.Text = null;
+            textBox7.Text = null;
+            textBox8.Text = null;
+        }
+    }
+}
